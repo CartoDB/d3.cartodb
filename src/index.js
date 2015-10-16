3028bad41460f6b088771e9a77625fa4d925bb0d
Francisco Dans <fdansv@gmail.com>
2015-10-16 17:59:36 +0200

    adds tileloader to browseridy

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-
-require('./leaflet_d3.js')
+require('./tileloader.js');
+require('./leaflet_d3.js');
 module.exports.d3 = {
 	Util: require('./util.js'),
 	Renderer: require("./renderer.js"),
 	jsonp: require('./d3.jsonp.js'),
-
 };
