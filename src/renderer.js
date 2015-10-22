69bec1fb7092bcdb707e092af54463ec96e75b9c
Francisco Dans <fdansv@gmail.com>
2015-10-22 15:59:21 +0200

    removes data querying from renderer

--- src/renderer.js
+++ src/renderer.js
@@ -16,8 +16,6 @@ var Renderer = function(options) {
   }
   this.globalVariables = {}
   this.user = options.user;
-  this.sql_api_template = options.sql_api_template || 'http://{user}.cartodb.com';
-  this.tileCache = {};
   this.layer = options.layer;
 }
 
@@ -41,7 +39,6 @@ Renderer.prototype = {
     } else {
       this.globalVariables = args[0]
     }
-    this._reset();
   },
 
   setCartoCSS: function(cartocss) {
@@ -50,89 +47,7 @@ Renderer.prototype = {
       this.shader = shader;
     }.bind(this));
   },
-
-  _reset: function(){
-
-  },
-
-  drawTile: function(tile, tilePoint, callback){
-    var tileData = this.tileCache[tilePoint.zoom + ":" + tilePoint.x + ":" + tilePoint.y];
-    if (tileData) {
-      this.render(tile, tileData, tilePoint);
-      callback(tilePoint, tile);
-    }
-    else{
-      function tile2lon(x,z) {
-        return (x/Math.pow(2,z)*360-180);
-      };
-      function tile2lat(y,z) {
-        var n=Math.PI-2*Math.PI*y/Math.pow(2,z);
-        return (180/Math.PI*Math.atan(0.5*(Math.exp(n)-Math.exp(-n))));
-      };
-      var tileBB = {
-        n: tile2lat(tilePoint.y, tilePoint.zoom),
-        s: tile2lat(tilePoint.y + 1, tilePoint.zoom),
-        e: tile2lon(tilePoint.x, tilePoint.zoom),
-        w: tile2lon(tilePoint.x + 1, tilePoint.zoom),
-      }
-      var query = "SELECT * FROM " + this.options.table;
-      query += " WHERE the_geom && ST_MakeEnvelope({w},{s},{e},{n}, 4326)";
-      query = query
-        .replace("{w}", tileBB.w)
-        .replace("{s}", tileBB.s)
-        .replace("{e}", tileBB.e)
-        .replace("{n}", tileBB.n);
-
-      this.getGeometry(query, tilePoint.zoom, function(geometry){
-        this.tileCache[tilePoint.zoom + ":" + tilePoint.x + ":" + tilePoint.y] = geometry;
-        this.render(tile, geometry, tilePoint);
-        callback(tilePoint, tile);
-      }.bind(this));
-    }
-  },
-
-  getGeometry: function(sql, zoom, callback) {
-    // request the schema fist to extract columns and generate the final
-    // sql query with the right the_geom simplification for the zoom level.
-    // The current zoom level may not the best but good enough for a test
-    var schemaSQL = 'select * from (' + sql + ') __cdb limit 0';
-    this._query(schemaSQL, function(data) {
-      // generate the final sql. Ideally only variables used in cartocss
-      // should be requested
-      var columns = Object.keys(data.fields).filter(function(f) {
-        return f !== 'the_geom' && f !== 'the_geom_webmercator';
-      });
-
-      // pixel size with some factor to avoid remove geometries
-      var px = this.pixelSizeForZoom(zoom);
-      var the_geom = 'st_transform(st_simplify(st_snaptogrid(the_geom_webmercator, {px}, {px}), {px}/2), 3857) as the_geom'.replace(/{px}/g, px);
-      // generate the sql with all the columns + the geometry simplified
-      var finalSQL = "select " + columns.join(',') + "," + the_geom + " FROM (" + sql + ") __cdb";
-
-      this._query(finalSQL, function(collection) {
-        collection.features = collection.features.filter(function(d) {
-          return d.geometry && d.geometry.coordinates.length > 0
-        })
-        callback(collection)
-      }, 'geojson');
-    }.bind(this));
-  },
-
-  _query: function(sql, callback, format) {
-    var url = this.sql_api_template.replace('{user}', this.user);
-    url += '/api/v2/sql?q=' + encodeURIComponent(sql);
-    if (format) {
-      url += "&format=" + format;
-    }
-    d3.json(url, callback);
-  },
-
-  pixelSizeForZoom: function(zoom) {
-    var earth_circumference = 40075017;
-    var tile_size = 256;
-    var full_resolution = earth_circumference/tile_size;
-    return full_resolution / Math.pow(2,zoom);
-  },
+  
 
   // there are special rules for layers, for example "::hover", this function
   // search for them and attach to the original layer, so if you have
