255e24a3019436f003e046b31573cfd53318b735
Francisco Dans <fdansv@gmail.com>
2015-11-18 17:25:57 +0100

    adds tilejson treatment in xyz provider

--- src/providers/xyz.js
+++ src/providers/xyz.js
@@ -5,6 +5,7 @@ function XYZProvider(options) {
   this.format = options.format;
   this.tileCache = {};
   this.urlTemplate = options.urlTemplate;
+  this.tilejson = options.tilejson;
 }
 
 XYZProvider.prototype = {
@@ -15,10 +16,15 @@ XYZProvider.prototype = {
       callback(tilePoint, tileData);
     }
     else{
+      if (!this.urlTemplate){
+        this.urlTemplate = this.tilejson.tiles[0];
+      }
       var url = this.urlTemplate
                 .replace("{x}", tilePoint.x)
                 .replace("{y}", tilePoint.y)
-                .replace("{z}", tilePoint.zoom);
+                .replace("{z}", tilePoint.zoom)
+                .replace("{s}", "a")
+                .replace(".png", ".geojson");
       this.getGeometry(url, function(err, geometry){
         if(geometry.type === "Topology"){
           self.format = "topojson";
@@ -29,7 +35,6 @@ XYZProvider.prototype = {
       }.bind(this));
     }
   },
-
   getGeometry: function(url, callback){
       d3.json(url, callback);
   },
