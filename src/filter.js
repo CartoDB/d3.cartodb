d6c85db37be1a9514e5eb1de8bc36a162bece0b3
Francisco Dans <fdansv@gmail.com>
2015-12-02 13:13:55 +0100

    indentation

--- src/filter.js
+++ src/filter.js
@@ -1,32 +1,33 @@
 var Crossfilter = require("crossfilter");
 
 function Filter(){
-	this.crossfilter = new Crossfilter();
-	this.dimensions = {};
-	this.tiles = new Set();
+  this.crossfilter = new Crossfilter();
+  this.dimensions = {};
+  this.tiles = new Set();
+  this.report = {};
 };
 
 Filter.prototype = {
-	addTile: function(tilePoint, collection){
-		if (this.tiles.has(tilePoint)) return;
-		this.crossfilter.add(collection.features.map(function(f){
+  addTile: function(tilePoint, collection){
+    if (this.tiles.has(tilePoint)) return;
+    this.crossfilter.add(collection.features.map(function(f){
       f.properties.tilePoint = tilePoint.zoom + ":" + tilePoint.x + ":" + tilePoint.y;
       return f;
     }));
     this.tiles.add(tilePoint);
-	},
+  },
 
-	removeTile: function(tilePoint){
-		if(!this.dimensions.tiles){
+  removeTile: function(tilePoint){
+    if(!this.dimensions.tiles){
       return;
     }
     this.dimensions.tiles.filter(tilePoint);
     this.crossfilter.remove();
     this.dimensions.tiles.filterAll();
-	},
+  },
 
-	getTile: function(tilePoint){
-		if(!this.dimensions.tiles){
+  getTile: function(tilePoint){
+    if(!this.dimensions.tiles){
       this.dimensions.tiles = this.crossfilter.dimension(function(f){return f.properties.tilePoint});
     }
     var tile = {type: "FeatureCollection", features: null};
