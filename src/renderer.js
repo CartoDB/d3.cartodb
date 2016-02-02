b3bda8ccc80b91754b8f36f12d830c501e51767e
Francisco Dans <fdansv@gmail.com>
2016-02-02 16:41:15 +0100

    alters examples

--- src/renderer.js
+++ src/renderer.js
@@ -23,8 +23,8 @@ var Renderer = function (options) {
   this.layer = options.layer
   this.filter = new Filter()
   this.geometries = {}
-  this.filter.on('featuresChanged', function(){
-    console.log("Features have changed")
+  this.filter.on('featuresChanged', function(features){
+    console.log("There are " + features.length + " features on the map")
   })
 }
 
